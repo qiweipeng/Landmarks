d8c065c551c5a7941c0c9b8c5e4c4b47b4e118ca
Weipeng Qi <qiweipeng@hotmail.com>
2021-04-30 11:47:56 +0800

    Create the project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Weipeng Qi on 2021/4/30.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
