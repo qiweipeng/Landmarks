9ccbaa79d16f63a3b009421b6fda189382352a19
Weipeng Qi <qiweipeng@hotmail.com>
2021-05-06 15:33:49 +0800

    Handling User Input

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,44 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Weipeng Qi on 2021/4/30.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+    }
+}
